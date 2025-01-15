bcd431e83bbeb318b7ce4e1cfc43ec0fe57c031f
P4Bs <pablojesus.mp@gmail.com>
2025-01-15 22:21:36 +0100

    rutas de controladores y actualizacion de vistas

--- ForoWebApp/Program.cs
+++ ForoWebApp/Program.cs
@@ -77,16 +77,16 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapStaticAssets();
 
 app.MapControllers();
 
