38ed22dfab01d8beffcef0d0a6afbd9ae6d7e4e1
P4Bs <pablojesus.mp@gmail.com>
2025-01-16 22:48:01 +0100

    Controlar los errores producidos en el registro o inicio de sesión

--- ForoWebApp/Models/RegistrationResult.cs
+++ ForoWebApp/Models/RegistrationResult.cs
@@ -2,9 +2,10 @@
 
 namespace ForoWebApp.Models
 {
-    public class RegistrationResult(bool success, User? user = null)
+    public class RegistrationResult(bool success, User? user = null, IEnumerable<string> errors = null)
     {
         public bool Success { get; set; } = success;
         public User User { get; set; } = user;
+        public IEnumerable<string> Errors { get; set; } = errors;
     }
 }
