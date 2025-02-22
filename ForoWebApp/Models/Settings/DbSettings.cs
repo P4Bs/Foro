bd3b88b1547d7cd9510b6836420f045f5e12bf1f
P4Bs <pablojesus.mp@gmail.com>
2025-02-22 08:17:06 +0100

    Set up environment variables

--- ForoWebApp/Models/Settings/DbSettings.cs
+++ ForoWebApp/Models/Settings/DbSettings.cs
@@ -1,7 +1,7 @@
 namespace ForoWebApp.Models.Settings;
 
-public class DbSettings
+public class DbSettings(string? connectionString = null, string? databaseName = null)
 {
-    public string ConnectionString { get; set; } = string.Empty;
-    public string DatabaseName { get; set; } = string.Empty;
+    public string ConnectionString { get; set; } = connectionString ?? string.Empty;
+    public string DatabaseName { get; set; } = databaseName ?? string.Empty;
 }
